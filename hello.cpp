e33f1ff344796177632bca10de4a14379142ca89
wangzhuolalala <wz7999829@gmail.com>
2025-10-08 22:01:50 +0800

    add a welcome branch

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+// 创建一个welcome函数
+void welcome() {
+    std::cout << "Welcome to the C++ world!" << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "HAHAHAHHA" << std::endl;
